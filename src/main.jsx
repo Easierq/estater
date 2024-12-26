09585cb0ea8338a1a504b2094fb878f07155b3aa
Easierq <isiaqofficial@gmail.com>
2024-12-26 17:15:38 +0100

    new updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <App />
-  </Router>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
