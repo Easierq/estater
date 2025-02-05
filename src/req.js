cb270d4ee05bcff6072c3c623ad7351dc6ecdc40
Easierq <isiaqofficial@gmail.com>
2025-02-05 15:05:46 +0100

    new updates

--- src/req.js
+++ src/req.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://estater-api.onrender.com/api";
+export const BASE_URL = "https://estater-api.onrender.com/api";
 // export const BASE_URL = "http://localhost:8800/api";
 
 const apiRequest = axios.create({
